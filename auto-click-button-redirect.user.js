e4115ea52acad314aa6ca76d6c855f0568b18813
Aztec-Rabbit <ars.xda@gmail.com>
2018-11-28 07:51:40 +0700

    Update auto-click-button-redirect.user.js

--- auto-click-button-redirect.user.js
+++ auto-click-button-redirect.user.js
@@ -16,11 +16,11 @@
     'use strict';
 
     function log(value) {
-        console.log("auto-click-button-redirect: " + value);
+        console.log('auto-click-button-redirect: ' + value);
     }
 
     function changeTitle() {
-        document.title = "Auto Click Button Redirect";
+        document.title = 'Auto Click Button Redirect';
     }
 
     function changeLink() {
@@ -41,7 +41,7 @@
 
     function pattern_1() {
         changeTitle();
-        var button = document.querySelector("div.humancheck>form>input.sorasubmit[value=Submit]");
+        var button = document.querySelector('div.humancheck>form>input.sorasubmit[value=Submit]');
         if (button) {
             button.click();
         } else {
@@ -52,7 +52,7 @@
     function pattern_2(element) {
         var button = document.querySelector(element);
         if (button) {
-            var url = button.getAttribute("href");
+            var url = button.getAttribute('href');
             if (url) {
                 changeTitle();
                 window.location.replace(url);
@@ -63,39 +63,39 @@
     function pattern_3(element) {
         var button = document.querySelector(element);
         if (button) {
-            var onclick = button.getAttribute("onclick");
+            var onclick = button.getAttribute('onclick');
             if (onclick) {
                 changeTitle();
-                eval(onclick.replace("_blank", "_self"));
+                eval(onclick.replace('_blank', '_self'));
             }
         }
     }
 
     switch (window.location.host) {
-        case "ljutkeunvpn.blogspot.com":
-        case "androidbusiness.us":
+        case 'ljutkeunvpn.blogspot.com':
+        case 'androidbusiness.us':
             pattern_0();
             break;
-        case "davinsurance.com":
-        case "sweetlantern.com":
-        case "lonelymoon.net":
+        case 'davinsurance.com':
+        case 'sweetlantern.com':
+        case 'lonelymoon.net':
             pattern_1();
             break;
-        case "www.greget.space":
-        case "www.tetew.info":
-            pattern_2("div.download-link>a");
+        case 'www.greget.space':
+        case 'www.tetew.info':
+            pattern_2('div.download-link>a');
             break;
-        case "spacetica.com":
-            pattern_2("div[align=center]>p>a.btn.btn-primary.btn-xs");
+        case 'spacetica.com':
+            pattern_2('div[align=center]>p>a.btn.btn-primary.btn-xs');
             break;
-        case "telolet.in":
-            pattern_2("#skip");
+        case 'telolet.in':
+            pattern_2('#skip');
             break;
-        case "giga74.com":
-            pattern_3("#d>a>div.button.green");
+        case 'giga74.com':
+            pattern_3('#d>a>div.button.green');
             break;
-        case "decrypt2.safelinkconverter.com":
-            pattern_3(".content_box>.decrypt>.redirect_url>div");
+        case 'decrypt2.safelinkconverter.com':
+            pattern_3('.content_box>.decrypt>.redirect_url>div');
             break;
 
         default: break;
